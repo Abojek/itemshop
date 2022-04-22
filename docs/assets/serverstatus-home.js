9a0bbbad0bf0b4b0f011bbf31b6329cfef31501f
Abojek <103895925+Abojek@users.noreply.github.com>
2022-04-22 21:20:38 +0200

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -8,7 +8,7 @@
         // siteNav dependent
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Zparaszamy do gry 🥰: ${data.players.online} Online</span>`;
         {% else %}
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Serwer włączony!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
